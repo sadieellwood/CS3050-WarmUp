654a45f049a8b5af1587ed02026745f6178598b8
sadieellwood <sellwood@uvm.edu>
2026-09-09 19:51:56 -0400

    data cleaning script and updated csv

---
+++ data_cleaning.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import ast
+
+movies = pd.DataFrame(pd.read_csv("movies_metadata.csv"))
+
+"""
+Handling dates. 
+- Mixed formatting in the release date column, eliminating rows with dates that contain a '-' so that it can
+be converted to datetime easier. All dates in this format are before 1900 anyway.
+- for relevancy purposes and also to cut down the dataset, any movies prior to 2000 will also be removed.
+"""
+movies = movies[~movies['release_date'].str.contains('-')]
+movies['release_date'] = pd.to_datetime(movies['release_date'], format='%m/%d/%Y', errors="coerce")
+movies = movies[movies['release_date'] > pd.to_datetime('2000-01-01')]
+
+#removing rows w/ null values (except for belongs_to_collection)
+movies = movies.dropna(subset=['release_date', 'title', 'vote_average'])
+
+#getting just the name of the series from the belongs_to_collection column (prev a dictionary containing additional info)
+inSeries = movies.dropna(subset=['belongs_to_collection'])
+inSeries['belongs_to_collection'] = inSeries['belongs_to_collection'].apply(ast.literal_eval)
+inSeries['belongs_to_collection'] = inSeries['belongs_to_collection'].str['name']
+
+noSeries = movies[movies['belongs_to_collection'].isna()]
+
+#getting equal parts inSeries and noSeries
+movies_clean = pd.concat([inSeries.sample(250, random_state=1), noSeries.sample(250, random_state=1)])
+movies_clean.columns = ['series', 'release_date', 'title', 'rating']
+movies_clean.to_csv("movies_data_clean.csv", index = False)
