baf270a2337540f6feb549071825d142277d2839
Alexia Barnes <alexiarbarnes@gmail.com>
2026-09-21 19:32:06 -0400

    Added ability to parse multi-condition queries.

--- parser.py
+++ parser.py
@@ -7,22 +7,24 @@ class Parser:
     TITLE = pp.CaselessKeyword("Title")
     RATING = pp.CaselessKeyword("Rating")
 
-    # define operators
-    AND, OR = pp.Keyword.using_each(["AND", "OR"])
-    comparison_op = pp.one_of("== < <= > >=")("operator")
+    # define operators + set results name for dict
+    logical_op = (pp.Keyword("AND") | pp.Keyword("OR"))("logical_op")
+    comparison_op = pp.one_of("== < <= > >=")("comparison_op")
 
     # define fields + set results name for dict
     field = (SERIES | DATE | TITLE | RATING)("field")
 
+    # handle multi-word value strings
+    unquoted_word = ~logical_op + pp.Word(pp.alphanums + ".:-()")
+    unquoted_val = pp.Combine(unquoted_word + pp.ZeroOrMore(pp.White(" ") + unquoted_word))
+
     # define value + set results name for dict
-    # value = (pp.Word(pp.alphanums + ".-") | pp.QuotedString("'"))("value")
+    value = (pp.QuotedString("'") | unquoted_val)("value")
 
-    value = (pp.QuotedString("'") | pp.rest_of_line())("value")
-
-    # TODO: Add functionality for conjoined expressions
     # define expression format
-    expr = field + comparison_op + value
-
+    expr = pp.Forward()
+    base_expr = pp.Group(field + comparison_op + value)
+    expr << base_expr("expr1") + pp.ZeroOrMore(logical_op + base_expr("expr2"))
 
     def parse(self, query):
         # parse query using defined query language
@@ -38,10 +40,10 @@ if __name__ == "__main__":
     test_queries = [
         "Date < 1995-10-30",
         "Title == 'Night at the Museum: Secret of the Tomb'",
-        "Rating >= 3.2",
-        "rating == 5.0",
-        "Title == Pirates Passage",
-        "Title == Loose Change: 2nd Edition"]
+        "rating > 5.0 AND rating < 3.0",
+        "Title == Magic & Bird: A Courtship of Rivals",
+        "Title == Loose Change: 2nd Edition",
+        "Title == Pokemon Ranger and the Temple of the Sea"]
 
     for query in test_queries:
         print(parser.parse(query))
